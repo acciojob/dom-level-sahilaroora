e02e74c2bee1084187ed25111fb225cccf66ad5c
sahilaroora <115200977+sahilaroora@users.noreply.github.com>
2025-09-17 10:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+const targetElement = document.getElementById('level');
+
+let level = 0;
+
+for (let current = targetElement; current != null; current = current.parentElement) {
+    level++;
+}
+
+alert("The level of the element is: " + level);
